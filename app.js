024fb71152bb03eb0ec83d5f75b9cc35e8a701d0
Fábio Rodrigues <fabio1203.fabio@gmail.com>
2026-05-18 02:22:21 -0300

    trocando URL

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-﻿const API_URL = "https://api-cronograma.vercel.app/";
+﻿const API_URL = "https://my-json-server.typicode.com/fabiorodrigues12223/projeto-semin-rio";
 
 const DIAS_SEMANA = [
   "Domingo",
